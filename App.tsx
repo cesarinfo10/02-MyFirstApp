e461b7ca2352369881088925d2c53b4552f51d29
César Sánchez <cesarinfo10@gmail.com>
2024-04-04 05:43:35 -0300

    Botones Incrementar

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { SafeAreaView, Text, View } from "react-native"
 import { HelloWordScreen } from "./src/presentation/screens/HelloWordScreen"
+import { CounterScreen } from './src/presentation/screens/CounterScreen';
 
 export const App = () => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <HelloWordScreen name="César Sánchez"/> 
+     { /*<HelloWordScreen name="César Sánchez"/>*/ }
+     <CounterScreen/>
       </SafeAreaView>
   )
   }
